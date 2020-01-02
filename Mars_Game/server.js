ab27e5cc901471ada56d6a3d26760e2b249f4002
Christian <32764514+neowara@users.noreply.github.com>
2020-01-02 22:13:46 +0100

    added free assets found on

--- Mars_Game/server.js
+++ Mars_Game/server.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 var players = {};
 
 app.use(express.static(__dirname + '/public'));
- 
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,7 +14,11 @@ io.on('connection', function (socket) {
   console.log('a user connected');
   socket.on('disconnect', function () {
     console.log('user disconnected');
-  // create a new player and add it to our players object
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+    // create a new player and add it to our players object
     players[socket.id] = {
       rotation: 0,
       x: Math.floor(Math.random() * 700) + 50,
@@ -26,9 +30,9 @@ io.on('connection', function (socket) {
     socket.emit('currentPlayers', players);
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
-      });
+  });
 });
- 
+
 server.listen(8081, function () {
   console.log(`Listening on ${server.address().port}`);
 });
