afce0b08905e2bc7e903cf6ae9224671cedd3f53
Christian <32764514+neowara@users.noreply.github.com>
2020-01-02 22:59:18 +0100

    added assets preload and assign socket.id to each player

--- Mars_Game/public/js/game.js
+++ Mars_Game/public/js/game.js
@@ -19,10 +19,22 @@ var config = {
    
   var game = new Phaser.Game(config);
    
-  function preload() {}
+  function preload() {	
+    this.load.image('ship1', 'assets/PNG/playerShip1_blue.png');
+  }
    
   function create() {
+    var self = this;
     this.socket = io();
+    this.socket.on('currentPlayers', function (players) {
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === self.socket.id) {
+          addPlayer(self, players[id]);
+        }
+      });
+    });
   }
    
   function update() {}
+
+  
