af2a5c3c20ddc91ea5ad42dcaf32dbbe79044ad1
Christian <32764514+neowara@users.noreply.github.com>
2020-01-03 01:57:37 +0100

    assigned assets to the first player, updated some cdn files, loaded images into the create function

--- Mars_Game/public/js/game.js
+++ Mars_Game/public/js/game.js
@@ -1,40 +1,50 @@
 var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        debug: false,
-        gravity: { y: 0 }
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 1280,
+  height:  720,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: {
+        y: 0
       }
-    },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    } 
-  };
-   
-  var game = new Phaser.Game(config);
-   
-  function preload() {	
-    this.load.image('ship1', 'assets/PNG/playerShip1_blue.png');
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
   }
-   
-  function create() {
-    var self = this;
-    this.socket = io();
-    this.socket.on('currentPlayers', function (players) {
-      Object.keys(players).forEach(function (id) {
-        if (players[id].playerId === self.socket.id) {
-          addPlayer(self, players[id]);
-        }
-      });
-    });
-  }
-   
-  function update() {}
+};
 
-  
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('ship', 'assets/PNG/playerShip1_blue.png');
+  this.load.image('ship', 'assets/playerShip1_blue.png');
+}
+
+function create() {
+  var self = this;
+  this.add.image(100, 50, 'ship');
+  this.socket = io();
+  this.socket.on('currentPlayers', function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      }
+    });
+  });
+}
+
+function update() {}
+
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(50, 50);
+
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
