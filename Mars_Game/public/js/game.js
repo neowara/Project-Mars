6bc77a9ce8a9d941f5738f927aa294c7f8c29927
Christian <32764514+neowara@users.noreply.github.com>
2020-01-03 02:25:20 +0100

    added moving background and infinite scrolling

--- Mars_Game/public/js/game.js
+++ Mars_Game/public/js/game.js
@@ -1,8 +1,8 @@
 var config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 1280,
-  height:  720,
+  width: 800,
+  height: 600,
   physics: {
     default: 'arcade',
     arcade: {
@@ -23,13 +23,17 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('ship', 'assets/PNG/playerShip1_blue.png');
-  this.load.image('ship', 'assets/playerShip1_blue.png');
+  this.load.image('bg', 'assets/Backgrounds/darkPurple.png');
 }
 
 function create() {
+
   var self = this;
+  this.add.image(0,0, 'bg').setOrigin(0);
+  this.bg = this.add.tileSprite(0, 0, 800, 600, 'bg', 'darkPurple.png').setOrigin(0);
   this.add.image(100, 50, 'ship');
   this.socket = io();
+
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
       if (players[id].playerId === self.socket.id) {
@@ -38,12 +42,18 @@ function create() {
     });
   });
 }
-
-function update() {}
+	
+function update() {
+    this.bg.tilePositionY += 5;
+}
 
 function addPlayer(self, playerInfo) {
-  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(50, 50);
-
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
+  }
   self.ship.setDrag(100);
   self.ship.setAngularDrag(100);
   self.ship.setMaxVelocity(200);
